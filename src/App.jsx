2082e0ffed01e32a2cf5866e64de6dc712ff79ac
imzero9 <skdud000322@naver.com>
2023-07-05 14:39:05 +0900

    ✨ Feat: 작성 페이지 내 텍스트 atom 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import GlobalStyles from './styles/GlobalStyles'
 import DateInput from './components/atoms/DateInput'
+import WriteTitle from './components/atoms/Title/WriteTitle'
+import WriteSubtitle from './components/atoms/Title/WriteSubtitle'
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
       <DateInput />
+      <WriteTitle />
+      <WriteSubtitle />
     </div>
   )
 }
