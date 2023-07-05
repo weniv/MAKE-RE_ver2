bcaa3597968eac787fcdcf165da465af3930da2e
imzero9 <skdud000322@naver.com>
2023-07-05 16:18:17 +0900

    🐛 Fix: 기존 소제목 태그 h4->label로 변경

--- src/components/atoms/Title/WriteSubtitle.jsx
+++ src/components/atoms/Title/WriteSubtitle.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { styled } from 'styled-components'
 
-export default function WriteSubtitle({ subtitle }) {
-  return <Title>{subtitle}</Title>
+export default function WriteSubtitle({ id, subtitle }) {
+  return <Title htmlFor={`subtitle-${id}`}>{subtitle}</Title>
 }
 
 WriteSubtitle.defaultProps = {
+  id: null,
   subtitle: '서브타이틀',
 }
 
 // style
-const Title = styled.h4`
+const Title = styled.label`
   color: var(--font-color);
   font-size: 20px;
   margin: 32px 0 24px;
