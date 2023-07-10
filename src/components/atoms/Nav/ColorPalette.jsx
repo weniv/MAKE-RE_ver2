27546220f0897f13874f45928e83e871ea19c76a
ujin16 <94890646+ujin16@users.noreply.github.com>
2023-07-10 18:44:20 +0900

    :sparkles: Add: 컬러팔레트 클릭시 메인색상 변경및 active버튼 box-shadow 추가

--- src/components/atoms/Nav/ColorPalette.jsx
+++ src/components/atoms/Nav/ColorPalette.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
-import { styled } from 'styled-components'
+import React, { useState, useRef, useEffect } from 'react'
+import styled, { css } from 'styled-components'
 
-export default function ColorPalette({ color }) {
-  return <Cont color={color}></Cont>
+export default function ColorPalette({ color, setColor }) {
+  const [isClick, setIsClick] = useState(false)
+
+  const handleClick = () => {
+    setColor(color)
+    setIsClick(true)
+  }
+
+  const colorRef = useRef()
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (colorRef.current && !colorRef.current.contains(e.target)) {
+        setIsClick(false)
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside)
+  }, [colorRef])
+
+  return (
+    <Cont
+      color={color}
+      onClick={handleClick}
+      isClick={isClick}
+      ref={colorRef}
+    ></Cont>
+  )
 }
 
-const Cont = styled.div`
+const Cont = styled.button`
   width: 30px;
   height: 30px;
   border-radius: 50%;
   background-color: ${({ color }) => (color ? color : 'var(--main-color)')};
+
+  ${(props) =>
+    props.isClick &&
+    css`
+      box-shadow: ${(props) => props.color} 0px 0px 8px;
+    `}
 `
