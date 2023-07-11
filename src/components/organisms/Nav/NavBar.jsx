7299a2b093a3b102ff7255712fd69fe38fe60a44
ujin16 <94890646+ujin16@users.noreply.github.com>
2023-07-11 16:43:01 +0900

    :sparkles: Fix: type에 따라 navBar 조건부 렌더링

--- src/components/organisms/Nav/NavBar.jsx
+++ src/components/organisms/Nav/NavBar.jsx
@@ -4,14 +4,14 @@ import NavList from '../../atoms/Nav/NavList'
 export default function NavBar({ type }) {
   return (
     <Nav>
-      <NavList listName="프로필" clicked={true} />
-      <NavList listName="자기소개서" />
-      <NavList listName="커리어" />
-      <NavList listName="프로젝트" />
-      <NavList listName="경험" />
-      <NavList listName="자격증" />
-      <NavList listName="학력" />
-      <NavList listName="추가 URL" />
+      <NavList listName="프로필" clicked={true} type={type} />
+      <NavList listName="자기소개서" type={type} />
+      <NavList listName="커리어" type={type} />
+      <NavList listName="프로젝트" type={type} />
+      <NavList listName="경험" type={type} />
+      <NavList listName="자격증" type={type} />
+      <NavList listName="학력" type={type} />
+      <NavList listName="추가 URL" type={type} />
     </Nav>
   )
 }
