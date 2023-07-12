c48a74bf04f92ba0f41f1cad39c23dd31ad0f14b
ujin16 <94890646+ujin16@users.noreply.github.com>
2023-07-12 14:20:28 +0900

    :art: Edit: remoteList배열  map 함수 사용해서 컴포넌트 생성
    git log

--- src/components/organisms/Nav/NavBar.jsx
+++ src/components/organisms/Nav/NavBar.jsx
@@ -1,17 +1,37 @@
 import { styled } from 'styled-components'
 import NavList from '../../atoms/Nav/NavList'
+import { useState } from 'react'
 
+const remoteList = [
+  '프로필',
+  '자기소개서',
+  '커리어',
+  '프로젝트',
+  '경험',
+  '자격증',
+  '학력',
+  '추가 URL',
+]
 export default function NavBar({ type }) {
+  const [clickIdx, setClickIdx] = useState(0)
+  const [isFill, setIsFill] = useState(false)
+
   return (
     <Nav>
-      <NavList listName="프로필" clicked={true} type={type} />
-      <NavList listName="자기소개서" type={type} />
-      <NavList listName="커리어" type={type} />
-      <NavList listName="프로젝트" type={type} />
-      <NavList listName="경험" type={type} />
-      <NavList listName="자격증" type={type} />
-      <NavList listName="학력" type={type} />
-      <NavList listName="추가 URL" type={type} />
+      {remoteList.map((item, idx) => {
+        return (
+          <NavList
+            clickIdx={clickIdx}
+            listName={item}
+            id={idx}
+            isFill={isFill}
+            type={type}
+            onClick={() => {
+              setClickIdx(idx)
+            }}
+          ></NavList>
+        )
+      })}
     </Nav>
   )
 }
@@ -21,7 +41,7 @@ const Nav = styled.nav`
   flex-direction: column;
   gap: 8px;
   padding: 20px;
-  background-color: var(-bg-color);
+  background-color: var(--bg-color);
   border-radius: 16px;
   box-shadow: 0px 4px 44px 0px rgba(0, 0, 0, 0.04);
 `
