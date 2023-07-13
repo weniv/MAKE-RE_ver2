805c7a31f9631f40afc4e9963b773aceabf7eda4
ujin16 <94890646+ujin16@users.noreply.github.com>
2023-07-13 15:47:51 +0900

    :sparkles: Feat: 기술 스택, GitHub 추가

--- src/components/templates/Profile/Profile.jsx
+++ src/components/templates/Profile/Profile.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import { styled } from 'styled-components'
-import { WriteTitle } from '../../atoms/Title'
+import { WriteSubtitle, WriteTitle } from '../../atoms/Title'
 import Layout from '../../organisms/Component/Layout'
 import LicatFace from '../../../assets/icon-liacat.svg'
-import DefaultInput from '../../atoms/Input/DefaultInput'
+import DefaultInput, { Input } from '../../atoms/Input/DefaultInput'
 import DropBox from '../../atoms/DropBox/DropBox'
-import { Label } from '../../atoms/Input/DefaultInput'
+import AddBtn from '../../atoms/Button/AddBtn'
+import MainBtn from '../../atoms/Button/MainBtn'
 
 export default function Profile() {
   const previousData = null
@@ -16,129 +17,171 @@ export default function Profile() {
 
   return (
     <Layout>
-      <WriteTitle
-        title="프로필"
-        description="대략 본인의 프로필 정보를 입력해달라는 내용의 문구"
-      />
-      <ProfileCont>
-        <button className="profileImg">
-          {true ? (
-            <span className="ir">프로필 이미지 업로드</span>
-          ) : (
-            <span className="ir">프로필 이미지 변경</span>
-          )}
-        </button>
-        <div>
-          <InputCont>
-            <DefaultInput
-              // id={id}
-              type="text"
-              placeholder="예) 홍길동"
-              width="220px"
-              marginRight="12px"
-              // value={inputData}
-              // onChange={(e) => {
-              //   setInputData(e.target.value)
-              // }}
-            >
-              이름
-            </DefaultInput>
-            <DefaultInput
-              // id={id}
-              type="text"
-              placeholder="예) Kildong Hong"
-              width="356px"
-              // value={inputData}
-              // onChange={(e) => {
-              //   setInputData(e.target.value)
-              // }}
-            >
-              영문 이름
-            </DefaultInput>
-          </InputCont>{' '}
-          <InputCont>
-            <DefaultInput
-              // id={id}
-              type="text"
-              placeholder="예) 010-1234-5678"
-              width="220px"
-              marginRight="12px"
-              // value={inputData}
-              // onChange={(e) => {
-              //   setInputData(e.target.value)
-              // }}
-            >
-              전화번호
-            </DefaultInput>
-          </InputCont>
-          <InputCont>
-            <DefaultInput
-              // id={id}
-              type="text"
-              placeholder="예) paul-lab"
-              width="220px"
-              marginRight="12px"
-              // value={inputData}
-              // onChange={(e) => {
-              //   setInputData(e.target.value)
-              // }}
-            >
-              이메일
-            </DefaultInput>
-            <span>@</span>
-            <DefaultInput
-              // id={id}
-              type="text"
-              placeholder="예) paul-lab"
-              width="200px"
-              marginRight="8px"
-              // value={inputData}
-              // onChange={(e) => {
-              //   setInputData(e.target.value)
-              // }}
-            />
+      <Section>
+        <WriteTitle
+          title="프로필"
+          description="대략 본인의 프로필 정보를 입력해달라는 내용의 문구"
+        />
+        <ProfileCont>
+          <button className="profileImg">
+            {true ? (
+              <span className="ir">프로필 이미지 업로드</span>
+            ) : (
+              <span className="ir">프로필 이미지 변경</span>
+            )}
+          </button>
+          <div>
+            <InputCont>
+              <DefaultInput
+                // id={id}
+                type="text"
+                placeholder="예) 홍길동"
+                width="220px"
+                marginRight="12px"
+                // value={inputData}
+                // onChange={(e) => {
+                //   setInputData(e.target.value)
+                // }}
+              >
+                이름
+              </DefaultInput>
+              <DefaultInput
+                // id={id}
+                type="text"
+                placeholder="예) Kildong Hong"
+                width="356px"
+                // value={inputData}
+                // onChange={(e) => {
+                //   setInputData(e.target.value)
+                // }}
+              >
+                영문 이름
+              </DefaultInput>
+            </InputCont>{' '}
+            <InputCont>
+              <DefaultInput
+                // id={id}
+                type="text"
+                placeholder="예) 010-1234-5678"
+                width="220px"
+                marginRight="12px"
+                // value={inputData}
+                // onChange={(e) => {
+                //   setInputData(e.target.value)
+                // }}
+              >
+                전화번호
+              </DefaultInput>
+            </InputCont>
+            <InputCont>
+              <DefaultInput
+                // id={id}
+                type="text"
+                placeholder="예) paul-lab"
+                width="220px"
+                marginRight="12px"
+                // value={inputData}
+                // onChange={(e) => {
+                //   setInputData(e.target.value)
+                // }}
+              >
+                이메일
+              </DefaultInput>
+              <span>@</span>
+              <DefaultInput
+                // id={id}
+                type="text"
+                placeholder="예) paul-lab"
+                width="200px"
+                marginRight="8px"
+                // value={inputData}
+                // onChange={(e) => {
+                //   setInputData(e.target.value)
+                // }}
+              />
+              <DropBox
+                width="131"
+                list={['naver.com', 'daum.net', 'gmail.com']}
+                isSelected={isSelected}
+                setIsSelected={setIsSelected}
+                selectedData={selectedData}
+                setSelectedData={setSelectedData}
+              />
+            </InputCont>
+            <InputCont>
+              <DefaultInput type="url">기술 블로그 링크</DefaultInput>
+            </InputCont>
+            <Label>경력</Label>
             <DropBox
-              width="131"
-              list={['naver.com', 'daum.net', 'gmail.com']}
+              width="179"
+              list={[
+                '신입',
+                '1년',
+                '2년',
+                '3년',
+                '4년',
+                '5년',
+                '6년',
+                '7년',
+                '8년',
+                '9년',
+                '10년',
+              ]}
               isSelected={isSelected}
               setIsSelected={setIsSelected}
               selectedData={selectedData}
               setSelectedData={setSelectedData}
             />
-          </InputCont>
-          <InputCont>
-            <DefaultInput type="url">기술 블로그 링크</DefaultInput>
-          </InputCont>
-          <CareerLabel>경력</CareerLabel>
-          <DropBox
-            width="179"
-            list={[
-              '신입',
-              '1년',
-              '2년',
-              '3년',
-              '4년',
-              '5년',
-              '6년',
-              '7년',
-              '8년',
-              '9년',
-              '10년',
-            ]}
-            isSelected={isSelected}
-            setIsSelected={setIsSelected}
-            selectedData={selectedData}
-            setSelectedData={setSelectedData}
-          />
-        </div>
-      </ProfileCont>
+          </div>
+        </ProfileCont>
+      </Section>
       <Line />
+      <Section>
+        <WriteSubtitle subtitle="기술 스택" />
+        <FlexBox>
+          <Input
+            // id={id}
+            type="text"
+            placeholder="예) Python"
+            width="260px"
+            // value={inputData}
+            // onChange={(e) => {
+            //   setInputData(e.target.value)
+            // }}
+          />
+          <AddBtn />
+        </FlexBox>
+      </Section>
+      <Line />
+      <Section>
+        <WriteSubtitle subtitle="GitHub" />
+        <GitHubCont>
+          <DefaultInput
+            // id={id}
+            type="text"
+            width="260px"
+            // value={inputData}
+            // onChange={(e) => {
+            //   setInputData(e.target.value)
+            // }}
+          >
+            GitHub ID
+          </DefaultInput>
+          <MainBtn type="preview">내 잔디 불러오기</MainBtn>
+        </GitHubCont>
+
+        <Label>Contributions</Label>
+        <CommitBox />
+      </Section>
     </Layout>
   )
 }
 
+const Section = styled.div`
+  padding: 0 52px;
+`
+
 const ProfileCont = styled.div`
-  margin-top: 52px;
   display: flex;
   align-items: flex-start;
   gap: 52px;
@@ -163,13 +206,36 @@ const InputCont = styled.div`
   }
 `
 
-const CareerLabel = styled(Label)`
-  margin-bttom: 8px;
+const Label = styled.label`
+  color: var(--gray-color);
+  font-size: 12px;
+  display: block;
+  margin-bottom: 8px;
 `
 
 const Line = styled.div`
   width: 890px;
   height: 2px;
   background: var(--hover-color);
-  margin: 40px 0 32px;
+  margin: 40px 0 0;
+`
+
+const FlexBox = styled.div`
+  display: flex;
+  gap: 8px;
+  align-items: bottom;
+`
+
+const GitHubCont = styled(FlexBox)`
+  margin-bottom: 20px;
+
+  button {
+    align-self: flex-end;
+  }
+`
+const CommitBox = styled.div`
+  height: 160px;
+  border-radius: 10px;
+  border: 1px solid var(--border-color);
+  background: var(--hover-color);
 `
