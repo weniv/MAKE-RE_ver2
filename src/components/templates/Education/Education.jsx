3b4f690a8129b63f69023c25756e30fbf7c1dde3
imzero9 <skdud000322@naver.com>
2023-07-19 17:59:36 +0900

    ✨ Feat: 교육 아이템 추가 삭제 기능 추가

--- src/components/templates/Education/Education.jsx
+++ src/components/templates/Education/Education.jsx
@@ -1,9 +1,49 @@
+import React, { useRef, useState } from 'react'
+import { addData } from '../../../utils'
 import { styled } from 'styled-components'
-import { Layout, Education as Item } from '../../organisms/Component'
+import { Layout, EduItem } from '../../organisms/Component'
 import { WriteTitle } from '../../atoms/Title'
 import { MainBtn } from '../../atoms/Button'
 
 export default function Education() {
+  const [eduData, setEduData] = useState([
+    {
+      id: 1,
+      title: '',
+      start: '',
+      end: '',
+      progress: false,
+    },
+  ])
+
+  const maxId = eduData.reduce(
+    (acc, cur) => {
+      return acc.id > cur.id ? acc : cur
+    },
+    { id: 0 }
+  ).id
+
+  const nextId = useRef(maxId)
+
+  const val = {
+    id: nextId.current,
+    title: '',
+    start: '',
+    end: '',
+    progress: false,
+  }
+
+  /** 교육 추가 */
+  const addEdu = () => {
+    nextId.current++
+    addData(nextId.current, val, eduData, setEduData)
+  }
+
+  /** 자격증 삭제 */
+  const deleteEdu = (idx) => {
+    console.log('자격증 삭제 버튼 클릭')
+    setEduData(eduData.filter((edu, i) => i !== idx))
+  }
   return (
     <Layout>
       <Section>
@@ -12,11 +52,21 @@ export default function Education() {
             title="교육"
             description="대략 본인의 교육을 입력해달라는 내용의 문구"
           />
-          <MainBtn>교육 추가하기</MainBtn>
+          <MainBtn onClick={addEdu}>교육 추가하기</MainBtn>
         </Header>
-        <Item kind="교육" />
-        <Item kind="교육" />
-        <Item kind="교육" />
+        <ItemList>
+          {eduData &&
+            eduData.map((edu, idx) => (
+              <EduItem
+                idx={idx}
+                edu={edu}
+                deleteEdu={() => deleteEdu(idx)}
+                eduData={eduData}
+                setEduData={setEduData}
+                key={idx}
+              />
+            ))}
+        </ItemList>
       </Section>
     </Layout>
   )
@@ -30,4 +80,10 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
+`
+
+const ItemList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
 `
