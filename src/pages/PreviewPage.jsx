7dcb9f6ce91512c1bc97856a767478ee61cbf4d4
min-bok <leehy0782@gmail.com>
2023-08-21 15:49:38 +0900

    ✨ Feat: 프로필 및 자기소개 section 미리보기

--- src/pages/PreviewPage.jsx
+++ src/pages/PreviewPage.jsx
@@ -1,5 +1,27 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
+import ProfilePreview from '../components/templates/Profile/ProfilePreview'
+import styled from 'styled-components'
+
+export const LocalContext = createContext(null)
 
 export default function PreviewPage() {
-  return <div>PreviewPage</div>
+  const getlocalData = () => {
+    const data = localStorage.getItem('resumeData')
+    return JSON.parse(data)
+  }
+  const [data, setData] = useState(getlocalData)
+
+  return (
+    <Layout>
+      <LocalContext.Provider value={{ data, setData }}>
+        <ProfilePreview />
+      </LocalContext.Provider>
+    </Layout>
+  )
 }
+
+const Layout = styled.div`
+  width: 810px;
+  margin: 0 auto;
+  padding: 90px 0;
+`
