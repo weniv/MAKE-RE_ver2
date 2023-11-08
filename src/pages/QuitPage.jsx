f7a40d367dbfa4a63cc00442a3e7a53f6b6c5688
imzero9 <skdud000322@naver.com>
2023-11-08 13:52:33 +0900

    💄 Style: 탈퇴 완료 페이지 UI 구현

---
+++ src/pages/QuitPage.jsx
@@ -0,0 +1,55 @@
+import { useNavigate } from 'react-router-dom'
+import styled from 'styled-components'
+import Header from '../components/organisms/Header/Header'
+import { MainBtn } from '../components/atoms/Button'
+
+export default function QuitPage() {
+  const navigate = useNavigate()
+
+  const moveMain = () => {
+    navigate('/MAKE-RE_ver2/')
+  }
+
+  return (
+    <>
+      <Header options={{ isCenter: true, isWhite: true }} />
+      <main>
+        <Wrap>
+          <img src="images/favicon.svg" alt="메이커리 파비콘" />
+          <Title>회원 탈퇴 완료</Title>
+          <Content>메이커리를 이용해 주셔서 감사합니다.</Content>
+          <MainBtn type="full" onClick={moveMain}>
+            메인으로
+          </MainBtn>
+        </Wrap>
+      </main>
+    </>
+  )
+}
+
+const Wrap = styled.div`
+  width: 322px;
+  margin: 120px auto 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+    width: 80px;
+    height: 80px;
+  }
+`
+
+const Title = styled.h2`
+  color: var(--surface-color);
+  font-size: 32px;
+  margin: 24px 0;
+  font-weight: 600;
+  line-height: 40px;
+`
+
+const Content = styled.p`
+  color: var(--gray-lv4-color);
+  margin-bottom: 24px;
+  line-height: 22px;
+`
