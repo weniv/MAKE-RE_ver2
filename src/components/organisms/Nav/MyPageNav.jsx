a859415e06f2a03578bc07334c46582967ab54f9
imzero9 <skdud000322@naver.com>
2023-11-09 13:06:55 +0900

    💄 Style: 마이페이지 네비게이션 UI 추가

---
+++ src/components/organisms/Nav/MyPageNav.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+export default function MyPageNav() {
+  const [activeNavItem, setActiveNavItem] = useState(0)
+
+  const handleNavItemClick = (index) => {
+    setActiveNavItem(index)
+  }
+
+  return (
+    <Aside>
+      <nav>
+        <NavList>
+          <NavItem
+            isActive={activeNavItem === 0}
+            onClick={() => handleNavItemClick(0)}
+          >
+            <Link to="/MAKE-RE_ver2/myresume">이력서 관리</Link>
+          </NavItem>
+          <NavItem
+            isActive={activeNavItem === 1}
+            onClick={() => handleNavItemClick(1)}
+          >
+            <Link to="/MAKE-RE_ver2/mypage">프로필 페이지</Link>
+          </NavItem>
+        </NavList>
+      </nav>
+    </Aside>
+  )
+}
+
+const Aside = styled.aside`
+  width: 280px;
+  padding: 20px;
+  border-radius: 16px;
+  background-color: var(--background-color);
+  box-shadow: 0px 4px 44px 0px rgba(0, 0, 0, 0.04);
+`
+
+const NavList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`
+
+const NavItem = styled.li`
+  border-radius: 10px;
+  font-size: 14px;
+  border: ${(props) => props.isActive && '2px solid var(--primary-color)'};
+  background-color: ${(props) => props.isActive && 'var(--gray-lv1-color)'};
+
+  &:hover {
+    background-color: var(--gray-lv1-color);
+  }
+
+  a {
+    display: inline-block;
+    width: 100%;
+    padding: 14px 16px;
+  }
+`
