956ea7bb339dcd67400b40969cbdfc9e78341522
min-bok <leehy0782@gmail.com>
2023-11-10 13:47:34 +0900

    🐛Fix: App.js 충돌 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import GlobalStyles from './styles/GlobalStyles'
-<<<<<<< HEAD
-import { WritePage, PreviewPage, QuitPage, LoginPage } from './pages'
-=======
 import {
   WritePage,
   PreviewPage,
   QuitPage,
   NotFoundPage,
   LandingPage,
-  MyResumePage
+  MyResumePage,
+  LoginPage,
+  SignupPage,
 } from './pages'
->>>>>>> dfc6804d5035dc70e11bc0e4c2015e7aa1e22070
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Providers from './context/Providers'
-import SignupPage from './pages/SignupPage'
 
 function App() {
   return (
@@ -26,13 +23,10 @@ function App() {
             <Route path="/MAKE-RE_ver2/write" element={<WritePage />} />
             <Route path="/MAKE-RE_ver2/preview" element={<PreviewPage />} />
             <Route path="/MAKE-RE_ver2/quit" element={<QuitPage />} />
-<<<<<<< HEAD
             <Route path="/MAKE-RE_ver2/login" element={<LoginPage />} />
-            <Route path="/MAKE-RE_ver2/signup" element={<SignupPage />}></Route>
-=======
+            <Route path="/MAKE-RE_ver2/signup" element={<SignupPage />} />
             <Route path="/MAKE-RE_ver2/myresume" element={<MyResumePage />} />
             <Route path="*" element={<NotFoundPage />} />
->>>>>>> dfc6804d5035dc70e11bc0e4c2015e7aa1e22070
           </Routes>
         </BrowserRouter>
       </Providers>
