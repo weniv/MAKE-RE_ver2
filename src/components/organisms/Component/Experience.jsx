99f1667d315f476ac5c658baf491485862ed4409
baekg <baekg.dev@gmail.com>
2023-11-22 14:12:31 +0900

    :sparkles: Feat: 경험 섹션 항목 추가

--- src/components/organisms/Component/Experience.jsx
+++ src/components/organisms/Component/Experience.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { styled } from 'styled-components'
+import { css, styled } from 'styled-components'
 import ComponentHeader from '../ComponentHeader/ComponentHeader'
 import DefaultInput from '../../atoms/Input/DefaultInput'
 import { DateInput } from '../../atoms/Input'
@@ -23,58 +23,88 @@ export default function Experience({
         title={exp.title || null}
         handleDelete={deleteExp}
       >
-        <DefaultInput
-          width="100%"
-          marginRight="0px"
-          id={`exp-name-${exp.id}`}
-          type="text"
-          name="title"
-          placeholder="예) 제 7회 주식회사 위니브 청년 ICT 해외봉사 참여"
-          inputData={exp.title}
-          onChange={(e) => {
-            updateData(e, idx, expData, setExpData)
-          }}
-        >
-          경험명
-        </DefaultInput>
-        <Period>
-          <DateInput
-            id="startDate"
-            name="startDate"
-            width="220px"
-            inputData={exp.startDate}
-            onChange={(e) => updateData(e, idx, expData, setExpData)}
-          >
-            시작일
-          </DateInput>
-          <span>~</span>
-          <DateInput
-            id="endDate"
-            name="endDate"
-            width="220px"
-            isStill={isStill}
-            inputData={exp.endDate}
-            onChange={(e) => updateData(e, idx, expData, setExpData)}
-          >
-            종료일
-          </DateInput>
-          <ProceedingBtn
-            name="inProgress"
-            value={isStill}
-            idx={exp.id}
-            type="exp"
-            inputData={exp.inProgress}
+        <Cont>
+          <DefaultInput
+            width="100%"
+            marginRight="0px"
+            id={`exp-name-${exp.id}`}
+            type="text"
+            name="title"
+            placeholder="예) 제 7회 주식회사 위니브 청년 ICT 해외봉사 참여"
+            inputData={exp.title}
             onChange={(e) => {
-              setExpData(
-                expData.map((el, i) =>
-                  i === idx ? { ...el, inProgress: !isStill } : el
-                )
-              )
-              setIsStill(!isStill)
+              updateData(e, idx, expData, setExpData)
             }}
-            isStill={isStill}
-          />
-        </Period>
+          >
+            경험명
+          </DefaultInput>
+          <DefaultInput
+            width="100%"
+            marginRight="0px"
+            id={`exp-content-${exp.id}`}
+            type="text"
+            name="content"
+            placeholder="예) 제 7회 주식회사 위니브 청년 ICT 해외봉사 참여"
+            inputData={exp.content}
+            onChange={(e) => {
+              updateData(e, idx, expData, setExpData)
+            }}
+          >
+            설명
+          </DefaultInput>
+          <DefaultInput
+            width="100%"
+            marginRight="0px"
+            id={`exp-link-${exp.id}`}
+            type="text"
+            name="link"
+            placeholder="예) www.paullab.co.kr"
+            inputData={exp.link}
+            onChange={(e) => {
+              updateData(e, idx, expData, setExpData)
+            }}
+          >
+            링크
+          </DefaultInput>
+          <Period>
+            <DateInput
+              id="startDate"
+              name="startDate"
+              width="220px"
+              inputData={exp.startDate}
+              onChange={(e) => updateData(e, idx, expData, setExpData)}
+            >
+              시작일
+            </DateInput>
+            <span>~</span>
+            <DateInput
+              id="endDate"
+              name="endDate"
+              width="220px"
+              isStill={isStill}
+              inputData={exp.endDate}
+              onChange={(e) => updateData(e, idx, expData, setExpData)}
+            >
+              종료일
+            </DateInput>
+            <ProceedingBtn
+              name="inProgress"
+              value={isStill}
+              idx={exp.id}
+              type="exp"
+              inputData={exp.inProgress}
+              onChange={(e) => {
+                setExpData(
+                  expData.map((el, i) =>
+                    i === idx ? { ...el, inProgress: !isStill } : el
+                  )
+                )
+                setIsStill(!isStill)
+              }}
+              isStill={isStill}
+            />
+          </Period>
+        </Cont>
       </ComponentHeader>
     </ExpItem>
   )
@@ -89,8 +119,13 @@ const ExpItem = styled.li`
 const Period = styled.div`
   color: var(--surface-color);
   display: flex;
-  margin: 25px 0 0;
   & span {
     margin: 30px 8px 0;
   }
+`
+
+const Cont = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
 `
