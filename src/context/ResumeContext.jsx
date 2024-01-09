ade9f4b6388b14440a03e9e6dd7d93f4ed2172a2
imzero9 <skdud000322@naver.com>
2024-01-09 18:21:20 +0900

    ✨ Feat: resumeContext  로컬스토리지 갱신 기능 추가

--- src/context/ResumeContext.jsx
+++ src/context/ResumeContext.jsx
@@ -1,14 +1,18 @@
-import React, { createContext, useRef, useState } from 'react'
-import { resumeData as initialData } from '../data/dummy'
+import React, { createContext, useRef, useState, useEffect } from 'react'
+import { resumeList as initialData } from '../data/dummy'
 import { remoteList } from '../data/dummy'
 
 export const ResumeContext = createContext(null)
 
-export default function ResumeProvider({ children }) {
-  const data = JSON.parse(localStorage.getItem('resumeData'))
-  const [resumeData, setResumeData] = useState(data ? data : initialData)
+export function ResumeProvider({ children }) {
+  const storedData = JSON.parse(localStorage.getItem('resumeData'))
+  const [resumeData, setResumeData] = useState(storedData || initialData)
   const [navList, setNavList] = useState(remoteList)
   const formRef = useRef(null)
+
+  useEffect(() => {
+    localStorage.setItem('resumeData', JSON.stringify(resumeData))
+  }, [resumeData])
 
   return (
     <>
@@ -20,3 +24,5 @@ export default function ResumeProvider({ children }) {
     </>
   )
 }
+
+export default ResumeContext
--- src/data/dummy.js
+++ src/data/dummy.js
@@ -1,4 +1,63 @@
-export const resumeData = {
+export const resumeList = [
+  {
+    id: 1,
+    name: '새로운 이력서',
+    profile: {
+      profileImg: 'https://api.mandarin.weniv.co.kr/1687337079735.png', // 프로필 이미지 테스트용
+      name: '',
+      enName: '',
+      phoneNumber: '',
+      fullEmail: '',
+      blog: '',
+      careerLength: 0,
+      skills: [],
+      github: '',
+      intro: '',
+    },
+    career: [
+      {
+        id: 1,
+        startData: '',
+        endData: '',
+        inProgress: false,
+        works: '',
+      },
+    ],
+    project: [
+      {
+        id: 1,
+        title: '',
+        demoLink: '',
+        githubLink: '',
+        snsLink: '',
+        outline: '',
+        people: '',
+        startDate: '',
+        endDate: '',
+        inProgress: false,
+        contributions: [''],
+        skills: [''],
+      },
+    ],
+    experience: [
+      {
+        id: 1,
+        title: '',
+        startDate: '',
+        endDate: '',
+        inProgress: false,
+      },
+    ],
+    certificate: [{ id: 1, title: '', date: '' }],
+    education: [
+      { id: 1, title: '', startDate: '', endDate: '', inProgress: false },
+    ],
+    url: [{ id: 1, title: '', link: '' }],
+  },
+]
+
+export const resumeItem = {
+  name: '새로운 이력서',
   profile: {
     profileImg: 'https://api.mandarin.weniv.co.kr/1687337079735.png', // 프로필 이미지 테스트용
     name: '',
@@ -62,3 +121,16 @@ export const remoteList = [
   { id: 7, title: '교육' },
   { id: 8, title: '추가 URL' },
 ]
+
+export const profileData = {
+  profileImg: 'https://api.mandarin.weniv.co.kr/1687337079735.png', // 프로필 이미지 테스트용
+  name: '',
+  enName: '',
+  phoneNumber: '',
+  fullEmail: '',
+  blog: '',
+  careerLength: 0,
+  skills: [],
+  github: '',
+  intro: '',
+}
