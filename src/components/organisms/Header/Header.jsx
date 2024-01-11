da4386f066b19ea9067f9e780a1664bfa7407871
imzero9 <skdud000322@naver.com>
2024-01-11 16:36:02 +0900

    🔥 Remove: 로그아웃 메뉴 아이템 제거

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -15,25 +15,29 @@ export default function Header({ options }) {
   const [isMenuOpen, setMenuOpen] = useState(false)
   const navigate = useNavigate()
 
-  const handleToggleMenu = () => {
-    setMenuOpen(!isMenuOpen)
-  }
+  // const handleToggleMenu = () => {
+  //   setMenuOpen(!isMenuOpen)
+  // }
 
   const moveWrite = () => {
     navigate('/myresume')
   }
 
   // 프로필 메뉴 외부 클릭 시 닫기
-  const menuRef = useRef(null)
+  // const menuRef = useRef(null)
 
-  useEffect(() => {
-    const handleClickOutside = (e) => {
-      if (menuRef.current && !menuRef.current.contains(e.target)) {
-        setMenuOpen(false)
-      }
-    }
-    document.addEventListener('mousedown', handleClickOutside)
-  }, [menuRef])
+  // useEffect(() => {
+  //   const handleClickOutside = (e) => {
+  //     if (menuRef.current && !menuRef.current.contains(e.target)) {
+  //       setMenuOpen(false)
+  //     }
+  //   }
+  //   document.addEventListener('mousedown', handleClickOutside)
+  // }, [menuRef])
+
+  const moveMypage = () => {
+    navigate('/myresume')
+  }
 
   return (
     <styles.HeaderCont isWhite={isWhite}>
@@ -69,11 +73,15 @@ export default function Header({ options }) {
               )}
               {hasProfile && (
                 <styles.ProfileBtn
-                  onClick={handleToggleMenu}
-                  isMenuOpen={isMenuOpen}
+                  onClick={moveMypage}
+                  // isMenuOpen={isMenuOpen}
                 >
-                  <img alt="마이프로필" src={profileImg || LicatFace} />
-                  {isMenuOpen && (
+                  <img
+                    alt="마이프로필"
+                    src={profileImg || LicatFace}
+                    // onClick={moveMypage}
+                  />
+                  {/* {isMenuOpen && (
                     <styles.MenuList ref={menuRef}>
                       <styles.MenuItem>
                         <Link to="/myresume">마이페이지</Link>
@@ -82,7 +90,7 @@ export default function Header({ options }) {
                         <button>로그아웃</button>
                       </styles.MenuItem>
                     </styles.MenuList>
-                  )}
+                  )} */}
                 </styles.ProfileBtn>
               )}
               <ToggleBtn />
