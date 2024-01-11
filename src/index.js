716928e79df554e1e7f00480f1337344679d3bf8
ujin16 <94890646+ujin16@users.noreply.github.com>
2024-01-11 17:57:25 +0900

    :sparkles: Add: hydrateRoot 설정

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import Modal from 'react-modal'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const container = document.getElementById('root')
+const root = ReactDOM.createRoot(container)
+
+if (container.hasChildNodes()) {
+  ReactDOM.hydrateRoot(
+    container,
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+} else {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+}
 
 Modal.setAppElement('#root')
