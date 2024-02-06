3ea143473241d29bbe57d5adef7f0f87591d7467
imzero9 <skdud000322@naver.com>
2024-02-06 16:58:44 +0900

    🔥 Remove: 헤더 메뉴 관련 안 쓰는 코드 제거

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import Logo from '../../../assets/Logo.svg'
 import ToggleBtn from '../../atoms/Button/ToggleBtn'
@@ -13,28 +12,11 @@ export default function Header({ options }) {
   )
   const profileImg = storedDefaultData.profileImg
   const { isCenter, hasCreate, hasProfile, isWhite } = options
-  const [isMenuOpen, setMenuOpen] = useState(false)
   const navigate = useNavigate()
-
-  // const handleToggleMenu = () => {
-  //   setMenuOpen(!isMenuOpen)
-  // }
 
   const moveWrite = () => {
     navigate('/myresume')
   }
-
-  // 프로필 메뉴 외부 클릭 시 닫기
-  // const menuRef = useRef(null)
-
-  // useEffect(() => {
-  //   const handleClickOutside = (e) => {
-  //     if (menuRef.current && !menuRef.current.contains(e.target)) {
-  //       setMenuOpen(false)
-  //     }
-  //   }
-  //   document.addEventListener('mousedown', handleClickOutside)
-  // }, [menuRef])
 
   const moveMypage = () => {
     navigate('/myresume')
@@ -73,25 +55,8 @@ export default function Header({ options }) {
                 </MainBtn>
               )}
               {hasProfile && (
-                <styles.ProfileBtn
-                  onClick={moveMypage}
-                  // isMenuOpen={isMenuOpen}
-                >
-                  <img
-                    alt="마이프로필"
-                    src={profileImg || LicatFace}
-                    // onClick={moveMypage}
-                  />
-                  {/* {isMenuOpen && (
-                    <styles.MenuList ref={menuRef}>
-                      <styles.MenuItem>
-                        <Link to="/myresume">마이페이지</Link>
-                      </styles.MenuItem>
-                      <styles.MenuItem>
-                        <button>로그아웃</button>
-                      </styles.MenuItem>
-                    </styles.MenuList>
-                  )} */}
+                <styles.ProfileBtn onClick={moveMypage}>
+                  <img alt="마이프로필" src={profileImg || LicatFace} />
                 </styles.ProfileBtn>
               )}
               <ToggleBtn />
